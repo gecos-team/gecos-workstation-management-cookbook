ee27708aa80bdd66d636678f30ae7ebe7a753b87
Roberto C. Morano <rcmova@gmail.com>
2014-03-23 21:40:23 +0100

    retabbed

--- metadata.rb
+++ metadata.rb
@@ -14,77 +14,77 @@ end
 
 # more complete input definition via json-schemas:
 network_resource_js = {
-    type: "object",
-    required: ["network_type"],
-    properties:
-    {
-        gateway: { type: "string" },
-        ip_address: { type:"string" },
-        netmask: { type: "string" },
-        network_type: { pattern: "(^wired$|wireless)", type: "string" },
-        use_dhcp: { type: "boolean" },
-        dns_server: {
-            type: "array",
-            minItems: 1,
-            uniqueItems: true,
-            items: {
-                type: "string"
-            }
-        },
-        users: {
-            type: "array",
-            minItems: 0,
-            uniqueItems: true,
-            items: {
-                type: "object",
-                required: ["username","network_type"],
-                properties: {
-                    username: { type: "string" },
-                    gateway: { type: "string" },
-                    ip_address: { type:"string" },
-                    netmask: { type: "string" },
-                    network_type: { pattern: "(^wired$|wireless|vpn|proxy)", type: "string" },
-                    use_dhcp: { type: "boolean" }
-                }
-            }
-        },
-        job_ids: {
-            type: "array",
-            minItems: 0,
-            uniqueItems: true,
-            items: {
-                type: "object",
-                required: ["id"],
-                properties: {
-                    id: { type: "string" },
-                    status: { type: "string" }
-                }
-            }
+  type: "object",
+  required: ["network_type"],
+  properties:
+  {
+    gateway: { type: "string" },
+    ip_address: { type:"string" },
+    netmask: { type: "string" },
+    network_type: { pattern: "(^wired$|wireless)", type: "string" },
+    use_dhcp: { type: "boolean" },
+    dns_server: {
+      type: "array",
+      minItems: 1,
+      uniqueItems: true,
+      items: {
+        type: "string"
+      }
+    },
+    users: {
+      type: "array",
+      minItems: 0,
+      uniqueItems: true,
+      items: {
+        type: "object",
+        required: ["username","network_type"],
+        properties: {
+          username: { type: "string" },
+          gateway: { type: "string" },
+          ip_address: { type:"string" },
+          netmask: { type: "string" },
+          network_type: { pattern: "(^wired$|wireless|vpn|proxy)", type: "string" },
+          use_dhcp: { type: "boolean" }
         }
+      }
+    },
+    job_ids: {
+      type: "array",
+      minItems: 0,
+      uniqueItems: true,
+      items: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: { type: "string" },
+          status: { type: "string" }
+        }
+      }
     }
+  }
 }
 
 
 complete_js = { 
-    description: "GECOS workstation management LWRPs json-schema",
-    id: "http://gecos-server/cookbooks/#{name}/#{version}/network-schema#",
-    required: ["gecos_ws_mgmt"],
-    type: "object",
-    properties: {
-        gecos_ws_mgmt: {
-            type: "object",
-            required: ["network_mgmt"],
-            properties: {
-                network_mgnt: {
-                    type: "object",
-                    required: ["network_res"],
-                    properties: {
-                        network_res: network_resource_js
-                    }
-                }
-            }
+  description: "GECOS workstation management LWRPs json-schema",
+  id: "http://gecos-server/cookbooks/#{name}/#{version}/network-schema#",
+  required: ["gecos_ws_mgmt"],
+  type: "object",
+  properties: {
+    gecos_ws_mgmt: {
+      type: "object",
+      required: ["network_mgmt"],
+      properties: {
+        network_mgnt: {
+          type: "object",
+          required: ["network_res"],
+          properties: {
+            network_res: network_resource_js
+          }
         }
+      }
     }
+  }
 }
 
 attribute 'json_schema',
