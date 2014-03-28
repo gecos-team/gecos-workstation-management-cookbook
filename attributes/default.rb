d995658a9e23da937954584858efca4cacaf955a
David Amian <damian@emergya.com>
2014-03-28 14:12:33 +0100

    Added default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,6 +10,8 @@ default[:gecos_ws_mgmt][:network_mgmt][:network_res][:job_ids] = []
 default[:gecos_ws_mgmt][:software_mgmt][:software_sources_res][:repo_list] = []
 default[:gecos_ws_mgmt][:software_mgmt][:software_sources_res][:job_ids] = []
 
+default[:gecos_ws_mgmt][:software_mgmt][:package_res] = []
+
 default[:gecos_ws_mgmt][:printers_mgmt][:printers_res][:printer_list] = []
 default[:gecos_ws_mgmt][:printers_mgmt][:printers_res][:job_ids] = []
 
