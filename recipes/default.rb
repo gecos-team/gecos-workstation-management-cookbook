ccfb5367db772d579bb4881d34d1a7bc3a176e1a
David Amian <damian@emergya.com>
2014-03-28 13:14:10 +0100

    Added all recipes into default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,8 @@
 # All rights reserved - EUPL License V 1.1
 # http://www.osor.eu/eupl
 #
+
+include_recipe software_mgmt
+include_recipe misc_mgmt
+include_recipe network_mgmt
+include_recipe users_mgmt
