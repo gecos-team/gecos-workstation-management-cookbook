fb9cce507fa1e52f7d771b0a079bceab354f4933
Andrés Muñoz <andresmvera@gmail.com>
2014-03-31 22:02:58 +0200

    Added empty default attributes to attributes/default.rb for :screensaver_res

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,13 @@
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:ip_address] = ''
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:gateway] = ''
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:netmask] = ''
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:dns_servers] = []
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:network_type] = 'wired'
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:use_dhcp] = true
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:users] = []
+default[:gecos_ws_mgmt][:network_mgmt][:network_res][:job_ids] = []
+
+default[:gecos_ws_mgmt][:software_mgmt][:software_sources_res][:repo_list] = []
 default[:gecos_ws_mgmt][:software_mgmt][:software_sources_res][:job_ids] = []
 
 default[:gecos_ws_mgmt][:software_mgmt][:package_res][:package_list] = []
