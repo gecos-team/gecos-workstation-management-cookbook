b4ce2900e76b39afd8eeaf21d955436a2d621309
David Amian Valle <damian@emergya.com>
2014-04-24 11:44:29 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Roberto C. Morano"
 maintainer_email  "rcmorano@emergya.com"
 license           "Apache 2.0"
 description       "Cookbook for GECOS workstations administration"
-version           "0.2.2"
+version           "0.2.3"
 
 depends "apt"
 
