954a41d88f9df739f0e464890381303d4526f93c
David Amian Valle <damian@emergya.com>
2014-04-24 12:12:01 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,14 +5,12 @@ default[:gecos_ws_mgmt][:network_mgmt][:network_res][:dns_servers] = []
 default[:gecos_ws_mgmt][:network_mgmt][:network_res][:network_type] = 'wired'
 default[:gecos_ws_mgmt][:network_mgmt][:network_res][:use_dhcp] = true
 default[:gecos_ws_mgmt][:network_mgmt][:network_res][:users] = []
-default[:gecos_ws_mgmt][:network_mgmt][:network_res][:job_ids] = []
 
 default[:gecos_ws_mgmt][:network_mgmt][:sssd_res][:domain_list] = []
 default[:gecos_ws_mgmt][:network_mgmt][:sssd_res][:workgroup] = ''
 default[:gecos_ws_mgmt][:network_mgmt][:sssd_res][:enabled] = false
 
 default[:gecos_ws_mgmt][:software_mgmt][:software_sources_res][:repo_list] = []
-default[:gecos_ws_mgmt][:software_mgmt][:software_sources_res][:job_ids] = []
 
 default[:gecos_ws_mgmt][:software_mgmt][:package_res][:package_list] = []
 default[:gecos_ws_mgmt][:software_mgmt][:package_res][:pkgs_to_remove] = []
@@ -23,7 +21,6 @@ default[:gecos_ws_mgmt][:software_mgmt][:app_config_res][:firefox_config] = {}
 default[:gecos_ws_mgmt][:software_mgmt][:app_config_res][:thunderbird_config] = {} 
 
 default[:gecos_ws_mgmt][:printers_mgmt][:printers_res][:printers_list] = []
-default[:gecos_ws_mgmt][:printers_mgmt][:printers_res][:job_ids] = []
 
 default[:gecos_ws_mgmt][:misc_mgmt][:auto_updates_res][:auto_updates_rules][:logout_update] = false
 default[:gecos_ws_mgmt][:misc_mgmt][:auto_updates_res][:auto_updates_rules][:start_update] = false
@@ -39,10 +36,8 @@ default[:gecos_ws_mgmt][:misc_mgmt][:local_groups_res][:groups_list] = []
 default[:gecos_ws_mgmt][:misc_mgmt][:local_admin_users_res][:local_admin_list] = []
 
 default[:gecos_ws_mgmt][:users_mgmt][:user_apps_autostart_res][:users] = []
-default[:gecos_ws_mgmt][:users_mgmt][:user_apps_autostart_res][:job_ids] = []
 
 default[:gecos_ws_mgmt][:users_mgmt][:user_shared_folders_res][:users] = []
-default[:gecos_ws_mgmt][:users_mgmt][:user_shared_folders_res][:job_ids] = []
 
 default[:gecos_ws_mgmt][:users_mgmt][:web_browser_res][:users] = []
 
@@ -59,8 +54,6 @@ default[:gecos_ws_mgmt][:users_mgmt][:desktop_control_res][:users] = []
 default[:gecos_ws_mgmt][:users_mgmt][:folder_sharing_res][:users] = []
 
 default[:gecos_ws_mgmt][:users_mgmt][:screensaver_res][:users] = []
-default[:gecos_ws_mgmt][:users_mgmt][:screensaver_res][:job_ids] = []
-
 
 default[:gecos_ws_mgmt][:users_mgmt][:user_mount_res][:users] = []
 
