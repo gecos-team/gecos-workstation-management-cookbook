e1f5d68d861b068dc8e347f44596003e69ad8d61
David Amian Valle <damian@emergya.com>
2014-05-07 12:34:07 +0200

    Modify delete node call delete method to same api

--- providers/gcc.rb
+++ providers/gcc.rb
@@ -55,7 +55,7 @@ action :setup do
     else
       if not new_resource.uri_gcc.nil? and not new_resource.gcc_nodename.nil? and not new_resource.gcc_username.nil? and not new_resource.gcc_pwd_user.nil? and not new_resource.gcc_selected_ou.nil?
         Chef::Log.info("GCC: Desenlazando cliente de GECOS Control Center")
-        response = RestClient.post new_resource.uri_gcc + '/delete/computer/', {'node_id' => new_resource.gcc_nodename,'ou_name'=>new_resource.gcc_selected_ou}.to_json, :user => new_resource.gcc_username, :password => new_resource.gcc_pwd_user, :content_type => :json, :accept => :json
+        response = RestClient.delete new_resource.uri_gcc + '/register/computer/', {'node_id' => new_resource.gcc_nodename,'ou_name'=>new_resource.gcc_selected_ou}.to_json, :user => new_resource.gcc_username, :password => new_resource.gcc_pwd_user, :content_type => :json, :accept => :json
         if not response.code.between?(200,299)
           raise 'The GCC URI not response' 
         end
