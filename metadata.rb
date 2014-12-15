770c84b2856bcc395377e2a0e2f371411632dc57
David Amian <damian@emergya.com>
2014-12-15 14:51:32 +0100

    Added email metadata

--- metadata.rb
+++ metadata.rb
@@ -580,6 +580,104 @@ web_browser_js = {
     }
   }
 }
+email_client_js = {
+  title: "Email Configuration",
+  title_es: "Configuración de email",
+  type: "object",
+  required: ["identity", "imap", "smtp"],
+  properties: {
+    users: {
+      type: "object",
+      title: "Users",
+      title_es: "Usuarios",
+      patternProperties: {
+        ".*" => { type: "object", title: "Username", title_es: "Nombre de usuario",
+          required: ["identity"],
+          properties: {  
+            identity: {
+              title: "Identity of the user",
+              title_es: "Identidad del usuario",
+              type: "object",
+              properties: {
+                name: {
+                  title: "Name",
+                  title_es: "Nombre",
+                  type: "string"
+                },
+                email: {
+                  title: "Email address",
+                  title_es: "Dirección de correo electrónico",
+                  # pattern: TODO !!!!
+                  type: "string"
+                }
+              }
+            },
+            imap: {
+              title: "IMAP server",
+              title_es: "Servidor IMAP",
+              type: "object",
+              properties: {
+                hostname: {
+                  title: "Hostname",
+                  title_es: "Hostname",
+                  # pattern: TODO !!!!
+                  type: "string"
+                },
+                port: {
+                  title: "Port",
+                  title_es: "Puerto",
+                  default: 143,
+                  # selector: 143 / Libre TODO !!!!
+                  type: "number"
+                },
+                username: {
+                  title: "Username",
+                  title_es: "Nombre de usuario",
+                  type: "string"
+                }
+              }
+            },
+            smtp: {
+              title: "SMTP server",
+              title_es: "Servidor SMTP",
+              type: "object",
+              properties: {
+                hostname: {
+                  title: "Hostname",
+                  title_es: "Hostname",
+                  # pattern: TODO !!!!
+                  type: "string"
+                },
+                port: {
+                  title: "Port",
+                  title_es: "Puerto",
+                  default: 25,
+                  # selector: 25 / 110 / Libre TODO !!!!
+                  type: "number"
+                },
+                username: {
+                  title: "Username",
+                  title_es: "Nombre de usuario",
+                  type: "string"
+                }
+              }
+            },
+            updated_by: updated_js
+          }
+        }
+      }
+    },
+    support_os: support_os_js.clone,
+    job_ids: {
+      type: "array",
+      minItems: 0,
+      uniqueItems: true,
+      items: {
+        type: "string"
+      }
+    }
+  }
+}
 
 user_alerts_js = {
   title: "User alert",
