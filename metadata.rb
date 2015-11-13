609cb28031d92b7f437bc6e9fe798f400a397419
Pablo Martín <goinnn@gmail.com>
2015-11-13 10:20:38 +0100

    Fix typo version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer        "GECOS Team"
 maintainer_email  "gecos@guadalinex.org"
 license           "Apache 2.0"
 description       "Cookbook for GECOS workstations administration"
-version           "0.4.13"
+version           "0.4.0"
 
 depends "apt"
 depends "chef-client"
