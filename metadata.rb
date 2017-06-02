d6dcef72600182330d4b201acdb41186ca825f7b
Alfonso E.M <alfonso@el-magnifico.org>
2017-06-02 12:39:59 +0200

    New version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer        "GECOS Team"
 maintainer_email  "gecos@guadalinex.org"
 license           "Apache 2.0"
 description       "Cookbook for GECOS workstations administration"
-version           "0.5.0"
+version           "0.5.1"
 
 depends "apt"
 depends "chef-client"
