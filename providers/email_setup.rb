21a32493e69fc15d8425ed15eacfeb8539691888
a.macias.paredes <amacias@gruposolutia.com>
2018-06-05 16:41:12 +0200

    Install addons feature added and examples

--- providers/email_setup.rb
+++ providers/email_setup.rb
@@ -8,9 +8,8 @@
 # All rights reserved - EUPL License V 1.1
 # http://www.osor.eu/eupl
 #
-require 'chef/mixin/shell_out'
-require 'securerandom'
-include Chef::Mixin::ShellOut
+require 'json'
+require 'uri'
 
 action :setup do
 
@@ -42,7 +41,7 @@ action :setup do
 
         # Check if the email must be configured
         Chef::Log.info("Check if the email must be configured")
-         if user.base.email_setup
+        if user.base.email_setup
 
           # Create GECOS profile if doesn't exists
           homedir = `eval echo ~#{username}`.gsub("\n","")
@@ -53,30 +52,30 @@ action :setup do
              owner username
              group gid
              mode '700'
-             action :create
-          end
+             action :nothing
+          end.run_action(:create)
 
           directory "#{homedir}/.cache" do
             mode '0755'
             owner username
             group gid
-            action :create
-          end
+            action :nothing
+          end.run_action(:create)
            
-           
+
           file '/tmp/.X99-lock' do
-            action :delete
+            action :nothing
             only_if { ::File.exists?("/tmp/.X99-lock")}
-          end
+          end.run_action(:delete)
 
           execute "Create GECOS Profile" do
               command "Xvfb :99.0 -ac & sleep 1; thunderbird -CreateProfile 'gecos #{homedir}/.thunderbird/gecos' --display=:99.0; killall Xvfb"
               not_if { ::File.exists?("#{homedir}/.thunderbird/gecos") }
               user username
               environment ({'HOME' => homedir, 'USER' => username})
-              action :run
+              action :nothing
               timeout 30
-          end
+          end.run_action(:run)
 
           ruby_block "Remove thunderbird default profile" do
               block do
@@ -132,8 +131,8 @@ action :setup do
 
               end
               only_if { user.base.default_email }
-              action :run
-          end
+              action :nothing
+          end.run_action(:run)
 
           # Prepare environment variables
           VariableManager.reset_environ()
@@ -195,25 +194,132 @@ action :setup do
             mode '0755'
             owner username
             group gid
-            action :create
+            action :nothing
             only_if { ::File.exist?("#{homedir}/.thunderbird/gecos") and previous_hash != current_hash }
-          end
+          end.run_action(:create)
            
           file "#{homedir}/.thunderbird/gecos/digest" do
             content current_hash
             mode '0700'
             owner username
             group gid
-            action :create
+            action :nothing
             only_if { ::File.exist?("#{homedir}/.thunderbird/gecos") }
-          end          
+          end.run_action(:create)       
 
-	end
+          # Extensions download
+          exdir = "#{homedir}/.thunderbird/gecos/extensions"
+
+          directory exdir do
+            owner username
+            group gid
+            action :nothing
+          end.run_action(:create)
+
+          directory "/var/cache/gecos" do
+            mode '0755'
+            action :nothing
+          end.run_action(:create)
+  
+          directory "/var/cache/gecos/email_setup" do
+            mode '0755'
+            action :nothing
+          end.run_action(:create)    
+           
+          data['plugins'].each do |plugin_data|
+            Chef::Log.info("Checking addon: #{plugin_data['name']}")
+            
+            uri = URI.parse(plugin_data['url'])
+            plugin_file_name = ::File.basename(uri.path)            
+            plugin_file = "/var/cache/gecos/email_setup/#{plugin_file_name}"
+            
+            
+            # Download extension if necessary
+            remote_file plugin_file do
+              source plugin_data['url']
+              action :nothing
+            end.run_action(:create_if_missing)
+            
+            # Get extension ID
+            xid = shell_out("unzip -qc #{plugin_file} install.rdf |  xmlstarlet sel \
+                            -N rdf=http://www.w3.org/1999/02/22-rdf-syntax-ns# \
+                            -N em=http://www.mozilla.org/2004/em-rdf# \
+                            -t -v \
+                            \"//rdf:Description[@about='urn:mozilla:install-manifest']/em:id\"").stdout
+
+            Chef::Log.info("email_setup.rb - Extension ID = #{xid}")
+            
+
+            # Check if the extension is already installed by
+            # querying firefox extensions databases
+            xfiles = [ "extensions.json", "extensions.sqlite", "extensions.rdf" ]        
+            installed = false
+
+            xfiles.each do |xfile|        
+              xf = "#{homedir}/.thunderbird/gecos/#{xfile}"
+              Chef::Log.debug("web_browser.rb - Extension file = #{xf}")
+              if ::File.exist?(xf)
+                installed = case xfile
+                  when /\.json$/i
+                    jfile = ::File.read(xf)
+                    addons = JSON.parse(jfile)["addons"]                
+                    Chef::Log.debug("web_browser.rb - JSON addons: #{addons}")
+                    addons.any?{|h| h["id"] == xid}
+                  when /\.sqlite$/
+                    db = SQLite3::Database.open(xf)
+                    addons = db.get_first_value("SELECT locale.name, locale.description, addon.version, addon.active, addon.id FROM addon 
+                      INNER JOIN locale on locale.id = addon.defaultLocale WHERE addon.type = 'extension' AND addon.id = '#{xid}'
+                      ORDER BY addon.active DESC, UPPER(locale.name)")
+                    Chef::Log.debug("web_browser.rb - SQLite addons: #{addons}")
+                    !addons.nil?
+                  else   
+                    !!::File.open(xf).read().match(xid) # operator !! forces true/false returned
+                  end # case xfile
+              end # if ::File.exist?(xf)
+              break if installed
+            end # xfiles.each
+            Chef::Log.info("email_setup.rb - Installed plugin? #{installed}")
+
+            if not installed
+              plugin_dir_temp = "#{plugin_file}_temp"
+              Chef::Log.info("email_setup.rb - Extract plugin file to temporal directory: #{plugin_file} --> #{plugin_dir_temp}")
+              bash "extract plugin #{plugin_file}" do
+                action :nothing
+                code <<-EOH
+                  rm -rf #{plugin_dir_temp}
+                  mkdir -p #{plugin_dir_temp}
+                  unzip -o #{plugin_file} -d #{plugin_dir_temp}
+                EOH
+              end.run_action(:run)
+
+              source = plugin_dir_temp
+              destination = "#{exdir}/#{xid}"
+          
+              Chef::Log.info("email_setup.rb - Move directory: #{source} --> #{destination}")
+              ::FileUtils.mkdir_p(exdir)
+              ::FileUtils.mv(source, destination)       
+
+
+     
+            end # if not installed
+
+
+
+          end # data['plugins'].each
+
+           
         
 
-
-
-      end
+          bash "chown #{username}" do
+            code <<-EOH 
+              chown -R #{username}:#{gid} #{homedir}/.thunderbird/gecos
+            EOH
+            action :nothing
+          end.run_action(:run)
+           
+	end # if user.base.email_setup
+        
+      end # users.each_key
     else
       Chef::Log.info("This resource is not support into your OS")
     end
